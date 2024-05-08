aab4844c7ee85c556b06124fcbc6d96b39c6b81e
Arch2775 <93007037+Arch2775@users.noreply.github.com>
2024-05-08 12:46:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,18 +32,6 @@ def read_inferences(file):
         return f.read()
 
 def main():
-    st.markdown(
-        """
-        <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
     st.title('📊📈 Quantitative and Qualitative Analysis of SEC-10K filings from EDGAR Database')
 
 
